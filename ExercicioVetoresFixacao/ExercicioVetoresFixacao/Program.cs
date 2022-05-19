9c5b5e043b21570d26c95c2ca5ac8d673b90e353
Paulo Vitor dos Santos <84245409+paulovsantos@users.noreply.github.com>
2022-05-19 12:15:06 -0300

    Add files via upload

---
+++ ExercicioVetoresFixacao/ExercicioVetoresFixacao/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExercicioVetoresFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quarto[] vet = new Quarto[10];
+
+            Console.Write("Quantos quartos deseja alugar? ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Quarto desejado: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vet[quarto] = new Quarto(nome, email);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vet[i]);
+                }
+
+            }
+        }
+    }
+}
+
---
+++ ExercicioVetoresFixacao/ExercicioVetoresFixacao/Quarto.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ExercicioVetoresFixacao
+{
+    class Quarto
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public Quarto (string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                +
+                Email;
+        }
+    }
+}
