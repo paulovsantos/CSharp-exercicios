2542960fa9f06aae99d4def3f1242a9886f12781
paulovsantos <paulo.vitors@hotmail.com>
2022-07-12 15:08:49 -0300

    Adicionando exercicio proposto Herança e Pofimorfismo

---
+++ ExercicioProposto/ExercicioProposto/Entities/importedProduct.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace ExercicioProposto.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFree { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+        public ImportedProduct(string name, double price, double customsFree)
+            : base (name, price)
+        {
+            CustomsFree = customsFree;
+        }
+        
+        public virtual double TotalPrice()
+        {
+            return Price + CustomsFree;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs free: $ "
+                + CustomsFree.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+
+    }
+}
---
+++ ExercicioProposto/ExercicioProposto/Program.cs
@@ -0,0 +1,52 @@
+﻿using ExercicioProposto.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioProposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+                Console.Write("Common, used or imported (c/u/i)?");
+                char op = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+
+                if (op == 'i')
+                {
+                    Console.Write("Customs free: ");
+                    double customsFree = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFree));
+                } if(op == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufactureDate));
+                } else
+                {
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS: ");
+            foreach(Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
